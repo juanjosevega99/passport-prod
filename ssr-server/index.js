5c0f368d10ab65b861736054630dbbf5b6512891
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-26 22:40:16 -0500

    implementing OAuth 2.0 with google

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -15,12 +15,17 @@ app.use(cookieParser());
 // Basic strategy
 require("./utils/auth/strategies/basic");
 
+// OAuth strategy
+require("./utils/auth/strategies/oauth");
+
 app.post("/auth/sign-in", async function (req, res, next) {
   passport.authenticate("basic", function (error, data) {
     try {
       if (error || !data) {
         next(boom.unauthorized());
       }
+
+      const { token, ...user } = data;
 
       req.login(data, { session: false }, async function (error) {
         if (error) {
@@ -58,9 +63,74 @@ app.post("/auth/sign-up", async function (req, res, next) {
 
 app.get("/movies", async function (req, res, next) {});
 
-app.post("/user-movies", async function (req, res, next) {});
+app.post("/user-movies", async function (req, res, next) {
+  try {
+    const { body: userMovie } = req;
+    const { token } = req.cookies;
 
-app.delete("/user-movies/:userMovieId", async function (req, res, next) {});
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "post",
+      data: userMovie,
+    });
+
+    if (status !== 201) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(201).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/user-movies/:userMovieId", async function (req, res, next) {
+  try {
+    const { userMovieId } = req.params;
+    const { token } = req.cookies;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "delete",
+    });
+
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get(
+  "/auth/google-oauth",
+  passport.authenticate("google-oauth", {
+    scope: ["email", "profile", "openId"],
+  })
+);
+
+app.get(
+  "/auth/google-oauth/callback",
+  passport.authenticate("google-oauth", { session: false }),
+  function (req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev,
+    });
+
+    res.status(200).json(user);
+  }
+);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
